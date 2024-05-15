840d9dad9bf42cffe3a37523def1a4f2db76c588
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-05-14 17:30:02 -0700

    Introduccion

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Hola Mundo!!!');
+var htmlBody = document.querySelector('body');
+console.log(htmlBody);
+var channel = 'webtoriales';
+console.log(channel);
+var year = 2024;
---
+++ index.ts
@@ -0,0 +1,13 @@
+console.log('Hola Mundo!!!');
+
+const htmlBody = document.querySelector('body');
+
+console.log(htmlBody);
+
+const channel = 'webtoriales';
+
+console.log(channel);
+
+let year = 2024;
+
+year = 2024;
